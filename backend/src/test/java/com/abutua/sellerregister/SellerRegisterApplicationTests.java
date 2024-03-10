57cf78f219c96e0514f13d0722785333dbe6b19f
Wilson-Barbosa <wilsond_barbosa@outlook.com>
2024-03-09 21:04:08 -0300

    first commit

---
+++ backend/src/test/java/com/abutua/sellerregister/SellerRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abutua.sellerregister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellerRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
