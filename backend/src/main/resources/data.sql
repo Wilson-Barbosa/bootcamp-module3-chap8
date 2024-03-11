db0221c47e57eaf5682aca2cb028c96e51b86594
Wilson-Barbosa <wilsond_barbosa@outlook.com>
2024-03-10 23:39:21 -0300

    closes #4 - add connection and seed to the h2 database

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,1 @@
+INSERT INTO SELLER_TBL(name, salary, bonus, gender) VALUES ('Paul of Tarsus', 6370.00, 20.50, 'male'), ('Jhennifer Latos', 4500.00, 64.19, 'female'), ('Glauco Todesco', 5812.21, 50.00, 'male');
